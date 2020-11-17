6728db1ca6241c3f80e2165c95338b83f235e324
Erik Somnäs <eriksomnas@Eriks-iMac.lan>
2020-11-17 17:25:20 +0100

    testing the class

--- js/app.js
+++ js/app.js
@@ -3,18 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
     //Picture, name, age, city, followers, likes, photos
 
     class Profile {
-        constructor() {
+        constructor(city) {
             this.picture = "",
             this.first_name = "",
             this.last_name = "",
             this.age = 0,
-            this.city,
+            this.city = city,
             this.followers = 0,
             this.likes = 0,
             this.photo = 0
         };
     }
 
-    let p1 = new Profile();
+    const p1 = new Profile("Stockholm");
     console.log(p1);
 })
